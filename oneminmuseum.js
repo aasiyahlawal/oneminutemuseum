aa7dcb69f3de32a6490b67509f7e435e368f3ca1
aasiyahlawal <aasiyahlawal1@gmail.com>
2026-08-28 23:34:39 +0100

    funfact() function
    Created some fun fact function that will display a fun fact to the user

--- oneminmuseum.js
+++ oneminmuseum.js
@@ -5,3 +5,11 @@ function date() {
 
 const button1 = document.getElementById("exhbit1");
 button1.addEventListener("click", date);
+
+function funfact1(){
+    console.log("A cloud weight around a million tonnes");
+}
+
+function funfact2(){
+    console.log("Spit will instantly harden super glue!!")
+}
