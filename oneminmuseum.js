18eaf67147704905c9ab7607fa97c2889eaf71cf
aasiyahlawal <aasiyahlawal1@gmail.com>
2026-08-29 22:49:53 +0100

    image()
    - Working(ish) version of image() function
    - Next steps add alt tag

--- oneminmuseum.js
+++ oneminmuseum.js
@@ -13,3 +13,13 @@ function funfact1(){
 function funfact2(){
     console.log("Spit will instantly harden super glue!!")
 }
+
+function image(){ /*append child adds multiple images when exhbit 2 button clicked */
+    const myImage = new Image(100, 200);
+    myImage.src = "starrynight.webp";
+    document.body.appendChild(myImage); 
+    document.getElementById("exhbit2").innerHTML = Image();
+}
+
+const button2 = document.getElementById("exhbit2")
+button2.addEventListener("click", image)
